009901823ce53da0f16ba2becba7e44b2f77c336
wuast94 <github@wuast24.de>
2025-04-20 22:22:38 +0200

    fix(docker): fix arm build

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,11 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 		host: '0.0.0.0' // Keep this for dev server accessibility in Docker
+	},
+	// Add build options for SSR
+	build: {
+		rollupOptions: {
+			external: ['ping'] // Treat 'ping' as external during build
+		}
 	}
 });
